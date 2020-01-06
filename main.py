f83d13bdf35ff487b5d8e6dc64ec8c6a181ba0d6
Julien-Fruteau <jfruteau@L400906.hq.corp.leroymerlin.com>
2020-01-06 17:28:40 +0100

    all tests passed :)

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import math
 # the standard input according to the problem statement.
 
 max_speed = int(input())
-# max_speed = 90  # km/h
 light_count = int(input())
 print("max_speed, light_count = ({}, {})".format(
     max_speed, light_count), file=sys.stderr)
@@ -17,18 +16,11 @@ for i in range(light_count):
           i, distance, duration), file=sys.stderr)
 
     for speed in list(tmp_speeds):
-        # print(speed)
         speedSec = (speed * 1000) / 3600
         timeToLight = distance / speedSec
-        # print("speed, timeToLight = {}, {}".format(speed, timeToLight),
-        #       file=sys.stderr)
         if math.floor(round(timeToLight / duration, 10)) % 2 == 1:
-            # print("remove speed = {}".format(speed), file=sys.stderr)
-            # if speed == 60:
-            #     print("THIS ONE IS A PB")
             tmp_speeds.pop(tmp_speeds.index(speed))
 
 print("tmp_speeds = {}".format(tmp_speeds), file=sys.stderr)
-# print("solution = {}".format(max(tmp_speeds)), file=sys.stderr)
 
 print(max(tmp_speeds))
