1c24edb4f94ba6341cf69d5465f97809c2f3992a
Julien-Fruteau <jfruteau@L400906.hq.corp.leroymerlin.com>
2020-01-06 16:47:02 +0100

    1st commit - idea 1 failing in Germany

--- main.py
+++ main.py
@@ -4,14 +4,29 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-speed = int(input())
+max_speed = int(input())
+# max_speed = 90  # km/h
 light_count = int(input())
-print("{} {}".format(speed, light_count), file=sys.stderr)
+print("max_speed, light_count = {} {}".format(
+    max_speed, light_count), file=sys.stderr)
+tmp_speeds = [speed for speed in range(1, max_speed + 1)]
+
 for i in range(light_count):
     distance, duration = [int(j) for j in input().split()]
-    print("{} {}".format(distance, duration), file=sys.stderr)
+    print("Feu {} : distance, duration = {} {}".format(
+          i, distance, duration), file=sys.stderr)
 
-# Write an action using print
-# To debug: print("Debug messages...", file=sys.stderr)
+    for speed in list(tmp_speeds):
+        # print(speed)
+        speedSec = (speed * 1000) / 3600
+        timeToLight = distance / speedSec
+        # print("speed, timeToLight = {}, {}".format(speed, timeToLight),
+        #       file=sys.stderr)
+        if math.floor(timeToLight / duration) % 2 == 1:
+            # print("remove speed = {}".format(speed), file=sys.stderr)
+            tmp_speeds.pop(tmp_speeds.index(speed))
 
-print(50)
+print("tmp_speeds = {}".format(tmp_speeds), file=sys.stderr)
+# print("solution = {}".format(max(tmp_speeds)), file=sys.stderr)
+
+print(max(tmp_speeds))
